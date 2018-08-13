44a9e2b4bf2e9d7efc2009aada0eacfc86677961
chavans <chavans@mskcc.org>
2018-08-13 15:01:02 -0400

    Change

--- get_VAF_from_IMPACT.R
+++ get_VAF_from_IMPACT.R
@@ -19,6 +19,7 @@ head(pathology_qc_metrics)
 query_file = args[1]
 sample_ids = fread(query_file, sep = '\t', header=FALSE)
 out_file = paste0(gsub(".txt","",query_file),"_res.txt")
+facets_file = paste0(gsub(".txt","",query_file),"_DMPs_for_FACETS_review.txt")
 
 head(sample_ids)
 names(sample_ids)[1]="DMP Sample ID"
@@ -31,14 +32,21 @@ if(length(missing_in_path) >=1){
   print(paste("Missing",missing_in_path))
 }
 
+
 res<-left_join(sample_ids,pathology_qc_metrics,by=c(`DMP Sample ID`="DMP_ASSAY_ID")) %>%
   dplyr::select(everything(sample_ids),Median_Exonic_Mutation_VAF,Median_Silent_Mutation_VAF)
 
 res <- res %>% mutate(`AF_>=0.10_Exonic`=ifelse(Median_Exonic_Mutation_VAF>=0.10,1,0), 
-                      `AF_>=0.10_Silent`=ifelse(Median_Silent_Mutation_VAF>=0.10,1,0),
-                      `AF_<0.20_Exonic`=ifelse((Median_Exonic_Mutation_VAF<0.20 | is.na(Median_Exonic_Mutation_VAF)==T),"Yes","No"))
+                                            `AF_>=0.10_Silent`=ifelse(Median_Silent_Mutation_VAF>=0.10,1,0),
+                                                                  `AF_<0.20_Exonic`=ifelse(Median_Exonic_Mutation_VAF<0.20,"Yes","No")) # | is.na(Median_Exonic_Mutation_VAF)==T
+
+res_fr = filter(res, `AF_<0.20_Exonic` == "Yes")
+print(res_fr)
+write.table(res_fr$`DMP Sample ID`,facets_file,sep="\t", row.names=F, quote=F,append=TRUE)
+
+
 res <- res %>% mutate(`AF_>=0.10_Exonic`=ifelse(sample_ids$`DMP Sample ID` %in% missing_in_path,"Missing_Data",`AF_>=0.10_Exonic`), 
-                      `AF_>=0.10_Silent`=ifelse(sample_ids$`DMP Sample ID` %in% missing_in_path,"Missing_Data",`AF_>=0.10_Silent`)
-)
+                                            `AF_>=0.10_Silent`=ifelse(sample_ids$`DMP Sample ID` %in% missing_in_path,"Missing_Data",`AF_>=0.10_Silent`))
 dim(res)
 write.table(res, out_file, sep="\t", row.names=F, quote=F)
+
