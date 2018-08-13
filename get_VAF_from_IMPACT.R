5fcdb61f1a414a5c08b892a3691335c9579bd6be
chavans <chavans@mskcc.org>
2018-08-13 14:31:39 -0400

    removed hardcoding for input file

--- get_VAF_from_IMPACT.R
+++ get_VAF_from_IMPACT.R
@@ -1,19 +1,23 @@
-#library
-library(data.table)
-library(plyr)
-library(dplyr)
-library(stringr)
+suppressPackageStartupMessages({
+  library(data.table)
+  library(plyr)
+  library(dplyr)
+  library(stringr)
+})
 specify_decimal <- function(x, k) format(round(x, k), nsmall=k)
 "%ni%"<-Negate("%in%")
 
-#data
-#Pathology
+args = commandArgs(TRUE)
+if (is.null(args) | length(args)<1) {
+  message("Usage: Rscript get_VAF_from_IMPACT.R sample.list")
+  quit()
+}
+
 pathology_qc_metrics<-fread("/ifs/res/taylorlab/chavans/WES_sample_selection/MASTER_SAMPLE_LIST_deid.txt")
 head(pathology_qc_metrics)
 
-query_file = '/ifs/res/taylorlab/chavans/WES_sample_selection/20180713DS_DMP_Solit_PatientSpecimen_Request_Form_v3.txt'
-              
-sample_ids = fread(query_file, sep = '\t',header=FALSE)
+query_file = args[1]
+sample_ids = fread(query_file, sep = '\t', header=FALSE)
 out_file = paste0(gsub(".txt","",query_file),"_res.txt")
 
 head(sample_ids)
@@ -22,9 +26,11 @@ names(sample_ids)[1]="DMP Sample ID"
 head(pathology_qc_metrics)
 sample_ids$`DMP Sample ID` 
 missing_in_path<-setdiff(unique(sample_ids$`DMP Sample ID`), unique(pathology_qc_metrics$DMP_ASSAY_ID))
-#head(missing_in_path)
-#length(missing_in_path) #P-0009450-T01-IM5
-print(paste("Missing",missing_in_path))
+
+if(length(missing_in_path) >=1){
+  print(paste("Missing",missing_in_path))
+}
+
 res<-left_join(sample_ids,pathology_qc_metrics,by=c(`DMP Sample ID`="DMP_ASSAY_ID")) %>%
   dplyr::select(everything(sample_ids),Median_Exonic_Mutation_VAF,Median_Silent_Mutation_VAF)
 
